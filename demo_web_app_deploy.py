54f7b57517930e5b25cab5059d1e048e948582db
Bruno Rodrigues Carloto <brunnocarlotosjob@gmail.com>
2024-10-26 22:26:06 -0300

    Update demo_web_app_deploy.py

--- demo_web_app_deploy.py
+++ demo_web_app_deploy.py
@@ -1,15 +1,18 @@
+# Importando bibliotecas de uso
 import pandas as pd
 import numpy as np
 from PIL import Image
 import pickle
 import streamlit as st
 
-# from modelo_regressao_linear_brasil import RegressaoLinearBRC
+# Comentando from modelo_regressao_linear_brasil import RegressaoLinearBRC
 
 #Indicando sidebar
 st.markdown('*__Observação: para mais informações acerca do projeto, clique na seta no canto esquerdo superior da tela__* ')
 
-#Indicando do que se trata a web app
+# --------------- Criação do sidebar ------------------
+
+# Indicando do que se trata a web app
 # foto = Image.open('bruno.carloto (2).png')
 # st.sidebar.image(foto, use_column_width=True)
 st.sidebar.subheader('Bruno Rodrigues Carloto')
@@ -24,6 +27,8 @@ st.sidebar.markdown("Redes Sociais :")
 st.sidebar.markdown("- [Linkedin](https://www.linkedin.com/in/bruno-rodrigues-carloto)")
 st.sidebar.markdown("- [Medium](https://medium.com/@brc-deep-analytics)")
 st.sidebar.markdown("- [Mercadados](https://brunnocarlotosjob.wixsite.com/mercadados)")
+
+# --------------- Página principal ------------------
     
 st.header('Regressão Linear BRC')
 st.markdown(' ')
@@ -34,17 +39,24 @@ st.markdown(' ')
 usuario =  st.text_input('Gostaria de saber seu nome.')
 st.markdown(' ')
 
+# Input do preço do feijão
 preco_feijao = st.number_input(f'{usuario}, defina o preço do feijão para que eu estime o preço do arroz.')
 
 # RegressãoLinear
 # with open('Modelo_RegressaoLinearBRC.pkl', 'rb') as f:
   #  modelo_carregado = pickle.load(f)
-        
+
+# Recebimento do preço em X
 X = preco_feijao
+
+# Aplicando a equação do modelo RegressaoLinearBRC treinado
 y = 5.564934583749978 + 0.9174417727028592 * X
+
 # y = modelo_carregado.predizer(X)
+
+# Predição
 y_ajustado = float(np.round(y, 2))
 
-#Aplicando a inteligência
+# Resultado
 st.subheader('Resultado')
 st.markdown(f'{usuario}, a {preco_feijao} reais, o preço do arroz é estimado em {y_ajustado} reais.')
